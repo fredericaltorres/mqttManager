11618d0fb362884e798ef7206efbc553830d7328
Frederic Torres <ftorres@emoneyadvisor.com>
2018-10-03 23:07:13 -0400

    Creation

---
+++ mqttConsole/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using mqttConsole;
+using MqttLib;
+
+namespace Sample
+{
+    class Program
+    {
+
+        static bool MustPublish = false;
+
+        static void Main(string[] args)
+        {
+            string connectionString = "tcp://m15.cloudmqtt.com:10989";
+            string username = "lvyugmmd";
+            string password = "9rqYbUiLLY2s";
+            const string channel = "/home/temperature" ;
+
+            Console.WriteLine("Starting MqttDotNet sample program.");
+            using(var mqttManager = new MQTTManager(connectionString, Guid.NewGuid().ToString(), username, password))
+            {
+                mqttManager.NotificationEvent += MqttManager_NotificationEvent;
+                mqttManager.MessageArrived += MqttManager_MessageArrived;
+                mqttManager.Start();
+                if(args.Length == 1)
+                {
+                    for(var i=0; i<2; i++)
+                    {
+                        mqttManager.Publish(channel, $"[${i.ToString("000")}]Yes it is working... source-computer:${Environment.MachineName}");
+                    }
+                    
+                }
+                else
+                {
+                    mqttManager.Subscribe(channel);
+                }
+                Console.WriteLine("Hit enter to end");
+                Console.ReadLine();
+            }
+        }
+
+        private static void MqttManager_MessageArrived(PublishArrivedArgs message)
+        {
+            Console.WriteLine($"Message Arrived {message.Topic}, {message.Payload}, {message.QualityOfService}");
+        }
+
+        private static void MqttManager_NotificationEvent(string message)
+        {
+            Console.WriteLine($">{message}");
+        }
+    }
+}
---
+++ mqttManagerLib/MQTTManager.cs
@@ -0,0 +1,87 @@
+﻿using MqttLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mqttConsole
+{
+    /// <summary>
+    /// Based on MQTT Server : https://www.cloudmqtt.com/docs-dotnet.html
+    /// Mqtt lib : https://github.com/stevenlovegrove/MqttDotNet
+    /// </summary>
+    public class MQTTManager : IDisposable
+    {
+        private string _connectionString;
+        private string _clientId;
+        private IMqtt _client;
+
+        public delegate void NotificationEventType(string message);
+        public delegate void MessageArrivedType(PublishArrivedArgs message);
+        public event NotificationEventType NotificationEvent;
+        public event MessageArrivedType MessageArrived;
+
+        private void Notify(string m)
+        {
+            if(NotificationEvent != null)
+                NotificationEvent(m);
+        }
+
+        public MQTTManager(string connectionString, string clientId, string userName, string password)
+        {
+            this._connectionString = connectionString;
+            this._clientId = clientId;
+            this._client = MqttClientFactory.CreateClient(connectionString, _clientId, userName, password);
+            this._client.Connected += new ConnectionDelegate(client_Connected);
+            this._client.ConnectionLost += new ConnectionDelegate(_client_ConnectionLost);
+            this._client.PublishArrived += new PublishArrivedDelegate(client_PublishArrived);
+        }
+
+        public void Start()
+        {
+            Notify("Client connecting");
+            this._client.Connect(true);
+        }
+
+        public void Stop()
+        {
+            if (this._client.IsConnected)
+            {
+                this.Notify("Client disconnecting");
+                this._client.Disconnect();
+                this.Notify("Client disconnected");
+            }
+        }
+
+        public void Subscribe(string channel)
+        {
+            this._client.Subscribe(channel, QoS.BestEfforts);
+        }
+
+        public void Publish(string channel, string message)
+        {
+            this._client.Publish(channel, message, QoS.BestEfforts, false);
+        }
+
+        void client_Connected(object sender, EventArgs e)
+        {
+            this.Notify("Client connected");
+        }
+        void _client_ConnectionLost(object sender, EventArgs e)
+        {
+            this.Notify("Client connection lost");
+        }
+        bool client_PublishArrived(object sender, PublishArrivedArgs e)
+        {
+            if(this.MessageArrived != null)
+                this.MessageArrived(e);
+            return true;
+        }
+
+        public void Dispose()
+        {
+            this.Stop();
+        }
+    }
+}
