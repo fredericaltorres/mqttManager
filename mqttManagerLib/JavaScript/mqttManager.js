fc1ad3eb7fb66b01e88e7e610e56888ea133c4cc
Frederic Torres <ftorres@emoneyadvisor.com>
2018-10-05 21:47:17 -0400

    Added mqttManager.js

---
+++ mqttManagerLib/JavaScript/mqttManager.js
@@ -0,0 +1,121 @@
+/*
+    mqttManager.js a wrapper/helper around MQTT.js for browser.
+    Developed to work with MQTT broker from CloudMqtt.com.
+
+    Dependencies:
+    mqtt.js
+        https://github.com/mqttjs/MQTT.js
+        c:\>npm install mqtt --save
+ */
+var mqtt = require('mqtt');
+
+const WEB_SOCKET_DEFAULT_PORT = 30989;
+
+class MqttManager {
+
+    _client  = null;
+    _options = null;
+
+    constructor(mqttUrl, username, password, webSocketPort = WEB_SOCKET_DEFAULT_PORT, clientId) {
+
+        if(!clientId)
+            clientId = 'mqttManager.js_' + Math.random().toString(16).substr(2, 8);
+
+        this._options = {
+            port: webSocketPort, // Required for WebSocket
+            useSSL: true,        // Required for WebSocket
+            host: mqttUrl,
+            clientId,
+            username,
+            password,
+            keepalive: 60,
+            reconnectPeriod: 1000,
+            protocolId: 'MQIsdp',
+            protocolVersion: 3,
+            clean: true,
+            encoding: 'utf8'
+        };
+    }
+    trace(m) {
+        console.log(`[MqttMgr]${m}`);
+    }
+    parseMessage(rawMessage, topic) {
+        const values = (""+rawMessage).split('|');
+        if(values.length === 2) {
+            return { clientId: values[0], message : values[1], topic };
+        }
+        else {
+            return { message : rawMessage, topic };
+        }
+    }
+    onReceivedMessage = (topic, message) => {
+        const parsedMessage = this.parseMessage(message, topic);
+        if(parsedMessage.clientId === this._options.clientId) {
+            return; // ignore message sent by this instance
+        }
+        this.trace(`RECEIVED:${parsedMessage.message}, ${parsedMessage.clientId}, ${parsedMessage.topic}`);
+        if(this.messageArrived) {
+            this.messageArrived(parsedMessage);
+        }
+    }
+    subscribe(channel)
+    {
+        return new Promise( (resolve, reject) => {
+            this.trace(`Subscribe ${channel}`);
+            this._client.subscribe(channel, (err) => {
+                if(err) {
+                    console.log(`Subscribe err:${err}`);
+                    reject(channel);
+                }
+                else {
+                    this.trace(`Subscribed ${channel}`);
+                    this._client.on('message', this.onReceivedMessage);
+                    resolve(channel);                    
+                }
+            });
+        });
+    }
+    buildMessage(message) {
+        return `${this._options.clientId}|${message}`;
+    }
+    publish(channel, message)
+    {
+        return new Promise( (resolve, reject) => {
+            this.trace(`Publishing message:${message}`);
+            // https://github.com/mqttjs/MQTT.js#publish
+            this._client.publish(channel, this.buildMessage(message), undefined, (err) => {
+                if(err) {
+                    this.trace(`Publishing failed message:${message}`);
+                    reject(message);
+                }
+                else {
+                    resolve(message);
+                }
+            });
+        });
+    }
+    start() {
+        return new Promise( (resolve, reject) => {
+
+            const optionsOkToTrace = Object.assign({password:'ok'}, this._options);
+            optionsOkToTrace.password = null;
+            this.trace(`Connecting options:${JSON.stringify(optionsOkToTrace)}`);
+
+            this._client = mqtt.connect(this._options.host, this._options);
+        
+            this._client.on('error', (err) => {
+                console.log(err);
+                reject(false);
+            });
+            this._client.on('connect', () => {
+                this.trace(`MQTT Connected to ${this._options.host}`);
+                resolve(true);
+            });
+        });
+    }
+    stop() {
+
+    }
+}
+
+export default MqttManager;
